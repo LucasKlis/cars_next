c2fcd1df722baaa8c8bc8f59df22f9035c197038
LucasKlis <lukaszklis.poczta@gmail.com>
2023-06-24 06:38:39 +0200

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="flex-1 pt-36 padding-x">
+                <h1 className="hero__title">
+                Znajdź szybko i z łatwością swoje wymarzone auto!
+                </h1>
+                <p className="hero__subtitle">
+                usprawnij swój samochód dzięki naszemu łatwemu procesowi rezerwacji.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
